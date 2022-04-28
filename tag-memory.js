527cf8072e93c19c99f95d4f92ddb8b4a1b9cef3
diablomd64 <47569354+diablomd64@users.noreply.github.com>
2022-04-28 09:39:53 +0200

    Updated to 1.0.1
    Add some configuration parameter to let the node use an external clock

--- tag-memory.js
+++ tag-memory.js
@@ -3,12 +3,32 @@ module.exports = function(RED) {
         RED.nodes.createNode(this,config);
         var node = this;
 		var memory_payload = null;
-		var intervalTimer = setInterval(function(){
-			var newMsg = { payload: memory_payload, topic: config.outtopic };
-			node.send(newMsg);
-			}, config.filter);
+		var intervalTimer = setInterval(function()
+		{
+			if(!config.extclock)
+			{
+				var newMsg = { payload: memory_payload, topic: config.outtopic };
+				node.send(newMsg);
+			}
+				
+		}, config.filter);
         node.on('input', function(msg) {
-			memory_payload = msg.payload;
+			if(config.extclock)
+			{
+				if(config.triggertopic !== "" && msg.topic == config.triggertopic)
+				{
+					var newMsg = { payload: memory_payload, topic: config.outtopic };
+					node.send(newMsg);
+				}
+				else
+				{
+					memory_payload = msg.payload;					
+				}
+			}
+			else
+			{
+				memory_payload = msg.payload;
+			}
         });
     }
 	
